1648550b1893770e923b9f646933b8a83d5da3d3
Beatrix Victoria <144822592+beatrixvv@users.noreply.github.com>
2024-10-11 15:55:06 +0800

    Added OCR and highlight feature

--- script.js
+++ script.js
@@ -13,6 +13,39 @@ const breakTime = document.getElementById("break");
 let outputArr = [];
 let pause = false;
 
+// OCR feature
+["dragenter", "dragover", "dragleave", "drop"].forEach((event) => {
+  input.addEventListener(event, (e) => e.preventDefault());
+});
+
+// Highlight on dragover
+input.addEventListener("dragover", () => {
+  input.classList.add("dragover");
+});
+input.addEventListener("dragleave", () => {
+  input.classList.remove("dragover");
+});
+
+// Drop event
+input.addEventListener("drop", (e) => {
+  input.classList.remove("dragover");
+
+  const files = e.dataTransfer.files;
+  if (files.length > 0) {
+    const file = files[0];
+
+    if (file.type.startsWith("image/")) {
+      // OCR
+      (async () => {
+        const worker = await Tesseract.createWorker("chi_sim");
+        const ret = await worker.recognize(file.name);
+        input.textContent = ret.data.text;
+        await worker.terminate();
+      })();
+    }
+  }
+});
+
 addButton.addEventListener("click", (e) => {
   addOutput();
 });
@@ -69,6 +102,10 @@ playButton.addEventListener("click", (e) => {
       // TTS
       let index = 0;
       const speakNextWord = () => {
+        // Remove highlight
+        if (index != 0) {
+          words[index - 1].parentElement.classList.remove("highlight");
+        }
         if (index < wordsArr.length) {
           const speech = new SpeechSynthesisUtterance(wordsArr[index]);
           speech.lang = "zh-CN";
@@ -78,6 +115,8 @@ playButton.addEventListener("click", (e) => {
             setTimeout(speakNextWord, breakTime.value * 1000);
           };
           window.speechSynthesis.speak(speech);
+          // Highlight the text
+          words[index].parentElement.classList.add("highlight");
         }
       };
       speakNextWord();
@@ -105,33 +144,37 @@ function clearOutput() {
 }
 
 function addOutput() {
+  const splitFunct = (str) => str.split(/\r?\n/);
   const inputText = input.value.trim();
+  const inputArr = splitFunct(inputText);
 
-  if (inputText) {
-    // Create close button
-    let button = document.createElement("button");
-    button.className = "close";
-    button.textContent = "x";
+  inputArr.forEach((input) => {
+    if (input) {
+      // Create close button
+      let button = document.createElement("button");
+      button.className = "close";
+      button.textContent = "x";
 
-    // Create word span
-    let word = document.createElement("span");
-    word.className = "words";
-    word.textContent = inputText;
+      // Create word span
+      let word = document.createElement("span");
+      word.className = "words";
+      word.textContent = input;
 
-    // Create word div
-    let div = document.createElement("div");
-    div.className = "words-container";
-    div.appendChild(word);
-    div.appendChild(button);
+      // Create word div
+      let div = document.createElement("div");
+      div.className = "words-container";
+      div.appendChild(word);
+      div.appendChild(button);
 
-    // Append new div
-    output.appendChild(div);
+      // Append new div
+      output.appendChild(div);
 
-    // Add close button functionality
-    button.addEventListener("click", (e) => {
-      e.target.offsetParent.remove();
-    });
-  }
+      // Add close button functionality
+      button.addEventListener("click", (e) => {
+        e.target.offsetParent.remove();
+      });
+    }
+  });
   clearInput();
 }
 
